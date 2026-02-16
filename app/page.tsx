4e87a96c33a200eeb89c760874e9cc55a32e896b
Jack Dennis <jack.dennis.jr@sbcglobal.net>
2026-02-15 20:49:46 -0600

    Fix typo in name

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl font-semibold tracking-tight">Jack Dennis Jr.</h1>
+      <h1 className="text-4xl font-semibold tracking-tight">Jack Dennis, Jr.</h1>
 
       <p className="mt-6 text-lg leading-7 text-neutral-700">
         I’m a software engineering leader and lifelong builder. This site is where I share what I’m learning,
